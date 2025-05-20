ada9e991072b84e66259920138f1145b76b836b0
doffnoname <baylajr@hotmail.com>
2025-05-20 16:03:54 +0000

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,33 @@
-# Écrivez votre code ici !
+#1. Définissez une variable `nom` contenant votre **nom** sous forme de chaîne de caractères.
+nom = "Bayla Marley"
+
+#2. Définissez une variable `age` contenant votre **âge** sous forme d'un entier.
+age = 25
+
+#3. Définissez une variable `taille` contenant votre **taille** sous forme d'un nombre à virgule flottante.
+taille = 1.96
+
+#4. Définissez une variable `est_etudiant` contenant une valeur booléenne et indiquant si vous êtes **étudiant ou non**.
+est_etudiant = False
+
+#5. Utilisez la fonction `print` pour afficher les valeurs des variables précédemment définies.
+#Les prints doivent être sous la forme suivantes : 
+#* "Nom: `valeur de la variable nom`"
+print ("Nom: ", (nom))
+#* "Age: `valeur de la variable age`"
+print ("Age:", (age))
+#* "Taille: `valeur de la variable taille`"
+print ("Taille:", (taille),"m")
+#* "Est étudiant: `valeur de la variable est_etudiant`"
+print ("Est étudiant:", (est_etudiant))
+
+#6. Utilisez la fonction `type` pour afficher avec la fonction `print` les types des variables précédemment définies.
+#Les prints doivent être sous la forme suivantes : 
+#* "Type nom: `type de la variable nom`"
+print ("Type nom:", type(nom))
+#* "Type age: `type de la variable age`"
+print ("Type age:", type(age))
+#* "Type taille: `type de la variable taille`"
+print ("Type Taille:", type(taille))
+#* "Type est étudiant: `type de la variable est_etudiant`"
+print ("Type est étudiant:", type(est_etudiant))
