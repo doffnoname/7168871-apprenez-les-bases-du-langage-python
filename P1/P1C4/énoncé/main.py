42063a3964da9b0e916f37e615fb121099869d45
doffnoname <baylajr@hotmail.com>
2025-05-20 14:50:37 +0000

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,10 @@
 # Écrivez votre code ici !
+nom = Bayla
+age = 25
+
+# Affichez "Je m'appelle Bayla et j'ai 25 ans.
+print (f"Je m'appelle {nom} et j'ai {age} ans.")
+
+# Affichez "Je m'appelle Bayla et j'ai 35 ans.
+age = age + 10
+print (f"Je m'appelle {nom} et j'ai {age} ans. ")
