982b3574b5dae2e0e8229bd6ec678382b332dbb1
doffnoname <baylajr@hotmail.com>
2025-07-22 14:13:57 +0000

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,15 @@
 # Écrivez votre code ici !
+nom = Phenix
+age = 15
+taille = 1.80
+est_etudiant = False
+
+print (f"Nom:", {nom})
+print (f"Age:", {age})
+print (f"Taille:", {taille})
+print (f"Est étudiant:", {est_etudiant})
+
+print (f"Type Nom:", type {nom})
+print (f"Type Age:", type {age})
+print (f"Type Taille:", type {taille})
+print (f"Type Est étudiant:", type {est_etudiant})
