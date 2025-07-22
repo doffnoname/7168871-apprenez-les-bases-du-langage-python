60bafe2006ef58ebb8072bf34a19091776c8d9a6
doffnoname <baylajr@hotmail.com>
2025-07-22 14:27:50 +0000

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,18 @@
-# Écrivez votre code ici !
+# Creation quatre variables !
+nom = Phenix
+age = 15
+taille = 1.86
+est_etudiant = False
+
+# Affichage quatre variables !
+print (f"Nom: {nom}")
+print (f"Age: {age} ans")
+print (f"Taille: {taille}m")
+print (f"Est étudiant: {est_etudiant}")
+
+# Affichage type quatre variables !
+print (f"Nom:", type (nom))
+print (f"Age:", type (ag))
+print (f"Taill:", type (taille))
+print (f"Est étudiant:", type (est_etudiant))
+
