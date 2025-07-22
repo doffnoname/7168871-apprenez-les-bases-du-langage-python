6d5ed2678789415be3e5a223f17f515c57913caf
doffnoname <baylajr@hotmail.com>
2025-07-22 12:50:29 +0000

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,7 @@
 # Écrivez votre code ici !
+nom = Jordan
+prenom = Iverson
+age = 45
+print (f"Je m'appelle {prenom} {nom} et j'ai {age} ans.")
+age2 = (age + 10)
+print (f"Je m'appelle {prenom} {nom} et j'ai {age2} ans maintenant.")
