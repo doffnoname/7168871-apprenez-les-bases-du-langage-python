a9e76e3c92bf77744a58742098e094a2f2b477ac
doffnoname <baylajr@hotmail.com>
2025-07-28 19:12:24 +0000

    Update main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,18 @@
-# Écrivez votre code ici !
+# Écrivez votre code ici ! Créez une liste nommée fruits contenant les éléments "pomme", "banane" et "orange".
+fruits = ["pomme", "banane", "orange"]
+
+# Ajoutez "kiwi" à la liste fruits.
+fruits.append("kiwi")
+print (fruits)
+# Supprimez "orange" de la liste fruits.
+fruits.remove("orange")
+print (fruits)
+# Modifiez le deuxième élément de la liste fruits en "ananas".
+fruits.insert(1, "ananas")
+print(fruits)
+# Affichez la longueur de la liste fruits
+print (len(fruits))
+
+#Triez la liste fruits par ordre alphabétique
+fruits.sort()
+print (fruits)
